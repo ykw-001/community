905d334098ab277d44e733300927035305cbc81a
ykw-001 <2562551895@qq.com>
2022-07-06 16:47:04 +0800

    验证码功能加入

---
+++ src/main/java/com/nowcoder/community/config/KaptchaConfig.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * 配置kapcha验证码
+ *
+ * @author ykw 2562551895@qq.com
+ * @version 2022/7/6 16:10
+ * @since JDK8
+ */
+@Configuration
+public class KaptchaConfig {
+
+    @Bean
+    public Producer kaptchaProducer(){
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width", "100");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        properties.setProperty("kaptcha.textproducer.font.color", "0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        // 设置干扰类型
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
